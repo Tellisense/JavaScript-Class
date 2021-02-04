3a863d06cd7d34b28b9941ab4c21af40674bf6c7
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-02-03 17:29:00 -0800

    saving selectors

---
+++ 19_Single_Selectors/app.js
@@ -0,0 +1,28 @@
+// let x = document.getElementById("kx-proxy-stius1a3a");
+
+// document.getElementById("kx-proxy-stius1a3a").id;
+// // "kx-proxy-stius1a3a"
+// document.getElementById("kx-proxy-stius1a3a").className;
+// // ""
+
+// let x = document.getElementById("test");
+
+// document.getElementById("test").style.borderRadius = "25px";
+// x.style.width = "400px";
+// x.style.height = "400px";
+// x.style.backgroundColor = "salmon";
+
+// let y = document.querySelector("#test");
+// y.style.margin = "100px";
+
+let myUl = document.querySelector("ul li");
+
+myUl.style.color = "salmon";
+
+//single selectors
+document.getElementById();
+document.querySelector(); //CSS selector
+
+//multiple selectors
+document.getElementsByClassName(); //returns a collection
+document.querySelectorAll(); //CSS selector - returns a collection
