6318c8f6e94a25b9a24e6c2ce45362b3d9bf8826
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-02-11 15:31:16 -0800

    added 34 35

---
+++ 33_removing_and replacing_elements/app2.js
@@ -0,0 +1,5 @@
+// 1.  change my to do list to "Mendel's ToDo list"
+// 2. change the colors scheme of the orange button"
+// 3. make every other li gray also change the text on the gray items
+// 4. add a title Attribute to "Justin's To Do list"
+// 5. replace the bottom button with a dropdown Element
