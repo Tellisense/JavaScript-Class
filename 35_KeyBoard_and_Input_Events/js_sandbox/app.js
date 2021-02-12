fdd6cf216f41a33a87623716f368a7c5bef147e8
Justin Rahimi <9245522+Tellisense@users.noreply.github.com>
2021-02-11 17:33:02 -0800

    added color game

--- 35_KeyBoard_and_Input_Events/js_sandbox/app.js
+++ 35_KeyBoard_and_Input_Events/js_sandbox/app.js
@@ -1,6 +1,7 @@
 const clearBtn = document.querySelector(".clear-tasks");
 const card = document.querySelector(".card");
-const heading = document.querySelector("h5");
+const heading = document.querySelector("h1");
+const body = document.querySelector("body");
 
 // click
 // clearBtn.addEventListener("click", runEvent);
@@ -20,8 +21,16 @@ const heading = document.querySelector("h5");
 // clearBtn.addEventListener("mouseout", runEvent);
 
 //mousemove
-card.addEventListener("mousemove", runEvent);
+// card.addEventListener("mousemove", runEvent);
 
+//body
+body.addEventListener("mousemove", runEvent);
+
+//Event Handler
 function runEvent(e) {
   console.log(`EVENT TYPE IS:  ${e.type}`);
+  let x = e.clientX;
+  let y = e.clientY;
+  heading.textContent = `x: ${x}  y: ${y}`;
+  body.style.backgroundColor = `rgb( ${x} , ${y}, 80)`;
 }
